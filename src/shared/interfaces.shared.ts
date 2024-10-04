9be510a9229c0355c69aaef761a38af7f6ed297f
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-10-04 14:01:38 +0700

    feat(minio for interface): min io for interface but not implement yet

--- src/shared/interfaces.shared.ts
+++ src/shared/interfaces.shared.ts
@@ -1,7 +1,9 @@
 import type { PrismaClient } from "@prisma/client";
 import type Redis from "ioredis";
+import type * as Minio from "minio";
 
 export interface Context {
-	db: PrismaClient;
-	redis: Redis;
+  db: PrismaClient;
+  redis: Redis;
+  minio: Minio.Client;
 }
