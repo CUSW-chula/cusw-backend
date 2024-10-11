52535bb7bd23c5e3037c6645024918109e1187ec
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-10-11 22:35:49 +0700

    feat(shared/utils/websocket.utils.ts): add websocket function easy to use

--- src/controllers/projects.controller.ts
+++ src/controllers/projects.controller.ts
@@ -1,16 +1,28 @@
 import { Elysia } from "elysia";
 import { ProjectService } from "../services/projects.service";
 import { type Context } from "../shared/interfaces.shared";
+import { WebSocket } from "../shared/utils/websocket.utils";
+import { Project } from "@prisma/client";
 
 export const ProjectController = new Elysia({ prefix: "/projects" })
-	.get("/", ({ db, redis }: Context) => {
+	.get("/", async ({ db, redis }: Context) => {
 		const projectService = new ProjectService(db, redis);
-		return projectService.getAllProjects();
+		const projects = await projectService.getAllProjects();
+		return projects;
 	})
 	.get(
 		"/:id",
-		({ params: { id }, db, redis }: Context & { params: { id: string } }) => {
+		async ({
+			params: { id },
+			db,
+			redis,
+		}: Context & { params: { id: string } }) => {
 			const projectService = new ProjectService(db, redis);
-			return projectService.getProjectById(id);
+			const project = await projectService.getProjectById(id);
+
+			// This is example how to use Socket
+			const websocket = new WebSocket<Project>();
+			if (project !== null) websocket.broadcast(project);
+			return project;
 		},
 	);
--- src/shared/utils/websocket.utils.ts
+++ src/shared/utils/websocket.utils.ts
@@ -1,13 +1,19 @@
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 3001 }); // WebSocket server on port 3001
+// See how to use boradcast in /controller/project.controller.ts
+export class WebSocket<T> {
+	private readonly wss: WebSocketServer;
 
-// Broadcast function to send data to all connected clients
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export const broadcast = (data: any) => {
-	wss.clients.forEach((client) => {
-		if (client.readyState === client.OPEN) {
-			client.send(JSON.stringify(data));
-		}
-	});
-};
+	constructor() {
+		this.wss = new WebSocketServer({ port: 3001 });
+	}
+
+	// Broadcast function to send data to all connected clients
+	broadcast = (data: Partial<T>) => {
+		this.wss.clients.forEach((client) => {
+			if (client.readyState === client.OPEN) {
+				client.send(JSON.stringify(data));
+			}
+		});
+	};
+}
