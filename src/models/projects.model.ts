4abf78cc06e6137336e65b5eee9e4e30fd645b15
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-10-12 16:50:19 +0700

    fix(project.model and users.model): improve code struct

--- src/models/projects.model.ts
+++ src/models/projects.model.ts
@@ -1,18 +1,18 @@
-import type { Project as PrismaProject } from "@prisma/client";
+import type { Project } from "@prisma/client";
 import { BaseModel } from "../core/model.core";
 
-export class ProjectModel extends BaseModel<PrismaProject> {
-	async findAll(): Promise<PrismaProject[]> {
+export class ProjectModel extends BaseModel<Project> {
+	async findAll(): Promise<Project[]> {
 		const projects = await this.getModel().project.findMany();
 		return projects;
 	}
 
-	async findById(id: string): Promise<PrismaProject | null> {
+	async findById(id: string): Promise<Project | null> {
 		const project = await this.getModel().project.findUnique({ where: { id } });
 		return project;
 	}
 
-	async create(data: Partial<PrismaProject>): Promise<PrismaProject> {
+	async create(data: Partial<Project>): Promise<Project> {
 		const createdProject = await this.getModel().project.create({
 			data: {
 				title: data.title ?? "",
@@ -27,10 +27,7 @@ export class ProjectModel extends BaseModel<PrismaProject> {
 		return createdProject;
 	}
 
-	async update(
-		id: string,
-		data: Partial<PrismaProject>,
-	): Promise<PrismaProject> {
+	async update(id: string, data: Partial<Project>): Promise<Project> {
 		const updatedUser = await this.getModel().project.update({
 			where: {
 				id: id,
@@ -39,7 +36,7 @@ export class ProjectModel extends BaseModel<PrismaProject> {
 		});
 		return updatedUser;
 	}
-	async delete(id: string): Promise<PrismaProject> {
+	async delete(id: string): Promise<Project> {
 		const deletedUser = await this.getModel().project.delete({
 			where: { id },
 		});
