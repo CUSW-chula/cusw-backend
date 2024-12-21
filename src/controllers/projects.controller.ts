62ffd2135ff783a96f6fa49d039922f5ff484054
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2024-12-21 20:16:00 +0700

    feat(project): create patch delete

--- src/controllers/projects.controller.ts
+++ src/controllers/projects.controller.ts
@@ -1,6 +1,9 @@
-import { Elysia } from "elysia";
+import { type Cookie, Elysia, t } from "elysia";
 import { ProjectService } from "../services/projects.service";
 import { type Context } from "../shared/interfaces.shared";
+import { ActivityService } from "../services/activity-logs.service";
+import { WebSocket } from "../shared/utils/websocket.utils";
+import { $Enums } from "@prisma/client";
 
 export const ProjectController = new Elysia({ prefix: "/projects" })
 	.get("/", async ({ db, redis }: Context) => {
@@ -18,5 +21,94 @@ export const ProjectController = new Elysia({ prefix: "/projects" })
 			const projectService = new ProjectService(db, redis);
 			const project = await projectService.getProjectById(id);
 			return project;
+		},
+	)
+	.post(
+		"/",
+		async ({
+			body,
+			db,
+			redis,
+			cookie: { session },
+		}: Context & {
+			body: {
+				title: string;
+				description: string;
+				startDate: Date;
+				endDate: Date;
+			};
+			cookie: { session: Cookie<string> };
+		}) => {
+			const projectService = new ProjectService(db, redis);
+			const activityService = new ActivityService(db, redis);
+			const userId = session.value;
+			const project = await projectService.createProject(body);
+			WebSocket.broadcast("project", project);
+			const createProjectActivity = await activityService.postActivity(
+				userId,
+				$Enums.ActivityAction.CREATED,
+				"this project",
+				userId,
+			);
+			WebSocket.broadcast("activity", createProjectActivity);
+			return Response.json(project, { status: 200 });
+		},
+		{
+			body: t.Object({
+				title: t.String(),
+				description: t.String(),
+				startDate: t.Date(),
+				endDate: t.Date(),
+			}),
+		},
+	)
+	.patch(
+		"/:id",
+		async ({
+			body,
+			db,
+			redis,
+		}: Context & {
+			params: { id: string };
+			body: {
+				projectId: string;
+				title: string;
+				description: string;
+			};
+			cookie: { session: Cookie<string> };
+		}) => {
+			const projectService = new ProjectService(db, redis);
+			const updatedProject = await projectService.updateProject(
+				body.projectId,
+				body.title,
+				body.description,
+			);
+			WebSocket.broadcast("project", updatedProject);
+			return updatedProject;
+		},
+		{
+			body: t.Object({
+				projectId: t.String(),
+				title: t.String(),
+				description: t.String(),
+			}),
+		},
+	)
+	.delete(
+		"/:id",
+		async ({
+			params: { id },
+			db,
+			redis,
+		}: Context & { params: { id: string } }) => {
+			const projectService = new ProjectService(db, redis);
+			const project = await projectService.deleteProject(id);
+			WebSocket.broadcast("project", project);
+			return project;
+		},
+		{
+			body: t.Object({
+				projectId: t.String(),
+			}),
 		},
 	);
--- src/services/projects.service.ts
+++ src/services/projects.service.ts
@@ -3,13 +3,42 @@ import { BaseService } from "../core/service.core";
 import { ProjectModel } from "../models/projects.model";
 import type Redis from "ioredis";
 import { NotFoundError } from "elysia";
+import {
+	NotFoundException,
+	ServerErrorException,
+	ValidationException,
+} from "../core/exception.core";
+import { TasksModel } from "../models/tasks.model";
+import { ActivityLogsModel } from "../models/activity-logs.model";
+import { CommentModel } from "../models/comment.model";
+import { EmojiModel } from "../models/emoji.model";
+import { FilesModel } from "../models/files.model";
+import { TaskTagModel } from "../models/task-tag.model";
+import { TasksAssignmentModel } from "../models/tasks-assignment.model";
+import { UserModel } from "../models/users.model";
 
 export class ProjectService extends BaseService<Project> {
 	private readonly projectModel: ProjectModel;
+	private readonly taskModel: TasksModel;
+	private readonly taskAssignmentModel: TasksAssignmentModel;
+	private readonly emojiModel: EmojiModel;
+	private readonly taskTagModel: TaskTagModel;
+	private readonly fileModel: FilesModel;
+	private readonly activitiesLogsModel: ActivityLogsModel;
+	private readonly commentModel: CommentModel;
+	private readonly userModel: UserModel;
 
 	constructor(prisma: PrismaClient, redis: Redis) {
 		super(redis, 60);
 		this.projectModel = new ProjectModel(prisma);
+		this.taskModel = new TasksModel(prisma);
+		this.taskAssignmentModel = new TasksAssignmentModel(prisma);
+		this.emojiModel = new EmojiModel(prisma);
+		this.taskTagModel = new TaskTagModel(prisma);
+		this.fileModel = new FilesModel(prisma);
+		this.activitiesLogsModel = new ActivityLogsModel(prisma);
+		this.commentModel = new CommentModel(prisma);
+		this.userModel = new UserModel(prisma);
 	}
 
 	async getAllProjects(): Promise<Project[]> {
@@ -32,6 +61,100 @@ export class ProjectService extends BaseService<Project> {
 		const project = await this.projectModel.findById(id);
 		if (!project) throw new NotFoundError("Project not found");
 		await this.setToCache(cacheKey, project);
+		return project;
+	}
+
+	async createProject(data: Partial<Project>): Promise<Project> {
+		if (data.title !== null) {
+			const newTask = {
+				title: data.title,
+				description: data.description,
+				startDate: data.startDate,
+				endDate: data.endDate,
+				expectedBudget: data.expectedBudget,
+				realBudget: data.realBudget,
+				usedBudget: data.usedBudget,
+			};
+			return await this.projectModel.create(newTask);
+		}
+		throw new ValidationException("Title cann't be null");
+	}
+
+	async updateProject(
+		projectId: string,
+		title: string,
+		description: string,
+	): Promise<Project> {
+		// Find the project by ID
+		const existingProject = await this.projectModel.findById(projectId);
+		if (!existingProject) throw new NotFoundException("Project not found");
+
+		// Get all roles associated with the project
+		const roles = await this.projectModel.findrole(projectId);
+
+		const OwnerId = roles?.find((role) => role.role === "ProjectOwner")?.userId;
+		if (!OwnerId) throw new NotFoundException("Project Owner not found");
+
+		const isUserExist = await this.userModel.findById(OwnerId);
+		if (!isUserExist) throw new NotFoundException("User not found");
+
+		// Prepare the updated project object
+		const updatedProject = {
+			...existingProject,
+			title,
+			description,
+		};
+
+		// Invalidate caches
+		await this.invalidateCache("projects:all");
+		await this.invalidateCache(`projects:project:${projectId}`);
+
+		// Update and return the project
+		return await this.projectModel.update(projectId, updatedProject);
+	}
+
+	async deleteProject(projectId: string): Promise<Project> {
+		// Check if the project exists
+		const project = await this.projectModel.findById(projectId);
+		if (!project) {
+			throw new NotFoundException(`Project with ID ${projectId} not found`);
+		}
+
+		try {
+			// Find all tasks associated with the project
+			const tasks = await this.taskModel.findByProjectId(projectId);
+
+			// Delete all related entities for each task
+			if (tasks && tasks.length > 0) {
+				for (const task of tasks) {
+					const taskId = task.id;
+
+					// Delete all task-related data
+					await this.taskAssignmentModel.deleteByTaskId(taskId);
+					await this.taskTagModel.deleteByTaskId(taskId);
+					await this.emojiModel.deleteByTaskId(taskId);
+					await this.fileModel.deleteByTaskId(taskId);
+					await this.activitiesLogsModel.deleteByTaskId(taskId);
+					await this.commentModel.deleteByTaskId(taskId);
+
+					// Delete the task itself
+					await this.taskModel.delete(taskId);
+
+					// Invalidate cache for the task
+					await this.invalidateCache(`tasks:${taskId}`);
+				}
+			}
+
+			// Invalidate cache related to the project
+			await this.invalidateCache(`projects:${projectId}`);
+			await this.invalidateCache("projects:all");
+
+			// Delete the project itself
+			await this.projectModel.delete(projectId);
+		} catch (error) {
+			throw new ServerErrorException(`Error deleting project`);
+		}
+
 		return project;
 	}
 }
