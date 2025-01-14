069f34aa66659f6dab561921f6c2be24995b4ead
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-01-14 22:39:57 +0700

    fix(sql file): delete old tag.sql

--- sql/tag.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO "task_assignments" ("id", "taskId", "userId") VALUES ('cm5uyuwoi000clri81tcg2274', 'cm5uydol10005lri8mpofzfqu', 'cm5uysrcm0006lri8brf9812i');
---
+++ sql/tags.sql
@@ -0,0 +1,4 @@
+INSERT INTO "tags" ("id", "name") VALUES ('cm5uybv2o0000lri8cq94f363', 'สิงหาคม');
+INSERT INTO "tags" ("id", "name") VALUES ('cm5uybv2o0001lri8nab9nlir', 'กันยายน');
+INSERT INTO "tags" ("id", "name") VALUES ('cm5uybv2o0002lri8nav9zkjy', 'ตุลาคม');
+INSERT INTO "tags" ("id", "name") VALUES ('cm5uybv2o0003lri8v5n4mz15', 'มกราคม');
