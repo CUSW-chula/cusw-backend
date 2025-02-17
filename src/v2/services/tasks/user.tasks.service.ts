e0d1f34ac34e54d887f66e51de41a6876feea821
ThanapatDonlachanon <Thanapat.dlc@gmail.com>
2025-02-17 14:14:27 +0700

    fix(status & assigned task): when assigned user it not change in ui

--- src/v2/services/tasks/user.tasks.service.ts
+++ src/v2/services/tasks/user.tasks.service.ts
@@ -71,11 +71,12 @@ export class UserTaskClassService extends TaskService {
 			taskId: taskId,
 			userId: userId,
 		});
-		await this.projectRoleModel.create({
-			projectId: isTaskExist.projectId,
-			userId: userId,
-			role: "Member",
-		});
+
+		// await this.projectRoleModel.create({
+		// 	projectId: isTaskExist.projectId,
+		// 	userId: userId,
+		// 	role: "Member",
+		// });
 		await this.invalidateCache(cacheKey);
 		await this.invalidateCache("tasks:all");
 		await this.invalidateCache(`tasks:${taskId}`);
