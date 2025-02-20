a07a00418f167f0d50be8bc7601a32e10e643501
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2025-02-20 18:43:41 +0700

    feat(assign task): owner can not assign himself to task

--- src/v2/services/tasks/user.tasks.service.ts
+++ src/v2/services/tasks/user.tasks.service.ts
@@ -51,6 +51,12 @@ export class UserTaskClassService extends TaskService {
 		const isTaskExist = await this.getTaskModel().findById(taskId);
 		if (!isTaskExist) throw new NotFoundException("Task not found");
 
+		const isProjectOwner = await this.projectRoleModel.findByProjectIdAndUserId(
+			isTaskExist.projectId,
+			userId,)
+		if (isProjectOwner?.role === "ProjectOwner")
+			throw new ValidationException("Owner can't assign task to himself");
+
 		// Check assigner is not the assignee
 		const isAssignerIsAssignee = isTaskExist.createdById === userId;
 		if (isAssignerIsAssignee)
