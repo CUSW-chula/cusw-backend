f24f9cf854e614f73597f43ebc4528176feaa966
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-02-22 04:46:58 +0700

    fix: index

--- src/index.ts
+++ src/index.ts
@@ -18,100 +18,100 @@ const prisma = new PrismaClient();
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
 const minioEndpoint = new URL(
-  process.env.MINIO_ENDPOINT || "http://localhost:9000"
+	process.env.MINIO_ENDPOINT || "http://localhost:9000",
 );
 const minioClient = new Minio.Client({
-  endPoint: minioEndpoint.hostname,
-  port: parseInt(minioEndpoint.port),
-  useSSL: false,
-  accessKey: process.env.MINIO_ACCESS_KEY ?? "",
-  secretKey: process.env.MINIO_SECRET_KEY ?? "",
+	endPoint: minioEndpoint.hostname,
+	port: parseInt(minioEndpoint.port),
+	useSSL: false,
+	accessKey: process.env.MINIO_ACCESS_KEY ?? "",
+	secretKey: process.env.MINIO_SECRET_KEY ?? "",
 });
 
 // Initialize Elysia app
 const app = new Elysia()
-  .use(swagger({ version: "1.0.0" }))
-  .use(
-    cors({
-      origin: [
-        "http://localhost:3000",
-        "https://cusw-workspace.sa.chula.ac.th",
-      ],
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-      credentials: true,
-    })
-  )
-  .use(
-    jwt({
-      name: "jwt",
-      secret: process.env.JWT_SECRET ?? "default token",
-    })
-  )
-  .decorate("db", prisma)
-  .decorate("redis", redis)
-  .decorate("minio", minioClient);
+	.use(swagger({ version: "1.0.0" }))
+	.use(
+		cors({
+			origin: [
+				"http://localhost:3000",
+				"https://cusw-workspace.sa.chula.ac.th",
+			],
+			methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+			credentials: true,
+		}),
+	)
+	.use(
+		jwt({
+			name: "jwt",
+			secret: process.env.JWT_SECRET ?? "default token",
+		}),
+	)
+	.decorate("db", prisma)
+	.decorate("redis", redis)
+	.decorate("minio", minioClient);
 
 // Route for signing a token
 app.get("sign/:email", async ({ jwt, params }) => {
-  const userService = new UserService(prisma, redis);
-  const userId = await userService.getUserByEmail(params.email);
-  if (!userId?.id) {
-    throw new Error("User ID is undefined");
-  }
-  const auth = await jwt.sign({ id: userId.id });
-  return `${auth}`;
+	const userService = new UserService(prisma, redis);
+	const userId = await userService.getUserByEmail(params.email);
+	if (!userId?.id) {
+		throw new Error("User ID is undefined");
+	}
+	const auth = await jwt.sign({ id: userId.id });
+	return `${auth}`;
 });
 
 // Middleware for handling authorization and grouping routes by version
 app.guard(
-  {
-    headers: t.Object({
-      authorization: t.TemplateLiteral("Bearer ${string}"),
-    }),
-  },
-  (app) =>
-    app
-      .error({ Exception })
-      .onBeforeHandle(
-        async ({
-          headers: { authorization },
-          jwt,
-          set,
-          cookie: { session },
-        }) => {
-          const token = authorization.split(" ")[1];
-          const user = await jwt.verify(token);
-          if (!user) {
-            throw new UnauthorizedException("Unauthorized");
-          }
-          set.status = 200;
-          session.set({
-            value: user.id,
-            httpOnly: true,
-            path: "/api",
-          });
-        }
-      )
-      .onError(({ set, error }) => {
-        if (error instanceof Exception) {
-          set.status = error.statusCode;
-          return Response.json(error.message, { status: error.statusCode });
-        }
-      })
-      .group("/api/v1", (api) => {
-        controllersV1.forEach((controller) => api.use(controller));
-        return api;
-      })
-      .group("/api/v2", (api) => {
-        controllersV2.forEach((controller) => api.use(controller));
-        return api;
-      })
+	{
+		headers: t.Object({
+			authorization: t.TemplateLiteral("Bearer ${string}"),
+		}),
+	},
+	(app) =>
+		app
+			.error({ Exception })
+			.onBeforeHandle(
+				async ({
+					headers: { authorization },
+					jwt,
+					set,
+					cookie: { session },
+				}) => {
+					const token = authorization.split(" ")[1];
+					const user = await jwt.verify(token);
+					if (!user) {
+						throw new UnauthorizedException("Unauthorized");
+					}
+					set.status = 200;
+					session.set({
+						value: user.id,
+						httpOnly: true,
+						path: "/api",
+					});
+				},
+			)
+			.onError(({ set, error }) => {
+				if (error instanceof Exception) {
+					set.status = error.statusCode;
+					return Response.json(error.message, { status: error.statusCode });
+				}
+			})
+			.group("/api/v1", (api) => {
+				controllersV1.forEach((controller) => api.use(controller));
+				return api;
+			})
+			.group("/api/v2", (api) => {
+				controllersV2.forEach((controller) => api.use(controller));
+				return api;
+			}),
 );
 
 // Start the server
 app.listen(4000);
 
 console.info(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
 console.info("🦊 API is running at http://localhost:4000/swagger");
