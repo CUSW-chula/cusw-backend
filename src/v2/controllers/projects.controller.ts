5c329105f560906ac4a5b56acf070d23ffb42500
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2025-06-19 14:38:35 +0700

    fix(del import): del not use import

--- src/v2/controllers/projects.controller.ts
+++ src/v2/controllers/projects.controller.ts
@@ -2,9 +2,7 @@ import { type Cookie, Elysia, t } from "elysia";
 import { ProjectService } from "../services/projects.service";
 import { Project, type Context } from "../../shared/interfaces.shared";
 import { WebSocket as WebSocket } from "../../shared/utils/websocket.utils";
-import { UserService } from "../services/users.service";
 import { PermissionException } from "../../core/exception.core";
-import { redis } from "bun";
 
 export const ProjectController = new Elysia({
 	prefix: "/projects",
